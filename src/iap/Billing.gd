ddfee0ed065e5f270454e73b81a9db9e696fc018
Your Name <silver1miner@gmail.com>
2023-01-30 17:49:46 -0800

    fix ios iap

--- src/iap/Billing.gd
+++ src/iap/Billing.gd
@@ -131,25 +131,25 @@ func ios_purchase(index: int) -> void:
 	ios_iap.set_auto_finish_transaction(true)
 	match index:
 		0:
-			var response = ios_iap.purchase("gems_60_99")
+			var response = ios_iap.purchase({"product_id": "gems_60_99"})
 			gem_value = 60
 			print("purchase attempted with result: ", response.status)
 			if response != OK:
 				print("error purchasing item")
 		1:
-			var response = ios_iap.purchase("gems_315_499")
+			var response = ios_iap.purchase({"product_id": "gems_315_499"})
 			gem_value = 315
 			print("purchase attempted with result: ", response.status)
 			if response != OK:
 				print("error purchasing item")
 		2:
-			var response = ios_iap.purchase("gems_630_999")
+			var response = ios_iap.purchase({"product_id": "gems_630_999"})
 			gem_value = 630
 			print("purchase attempted with result: ", response.status)
 			if response != OK:
 				print("error purchasing item")
 		3:
-			var response = ios_iap.purchase("gems_1650_1999")
+			var response = ios_iap.purchase({"product_id": "gems_1650_1999"})
 			gem_value = 1650
 			print("purchase attempted with result: ", response.status)
 			if response != OK:
